620bba13f4ee39e9263164cb92ae68ca4b28ce2d
Oliver <onkelzfreak1988@hotmail.com>
2012-07-05 08:08:43 +0200

    update is_email function

--- system/library/functions.php
+++ system/library/functions.php
@@ -358,8 +358,8 @@
 	/**
 	 * Return true if the email is valid
 	 */
-	function is_email( $string ){
-		return eregi( "^[[:alnum:]][a-z0-9_.-]*@[a-z0-9.-]+\.[a-z]{2,4}$", $string );
+	function is_email( $email ){
+		return preg_match('|^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$|i', $email );
 	}
 
 	/**
