bf867834853a38ccfbcb350eb5d2c9d016176328
faryshta <angeldelcaos@gmail.com>
2012-09-24 16:47:40 -0500

    cleaning the bootstraps.php, passing some config instructions to index.php

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -10,27 +10,3 @@ $loader->auth_user();
 //$loader->auth_user();
 $loader->init_theme();//set theme
 $loader->init_js();
-
-#--------------------------------
-# Auto Load the Controller
-# init_route set the controller/action/params
-# to load the controller
-#--------------------------------
-$loader->auto_load_controller();
-
-#--------------------------------
-# Load model
-# load the model and assign the result
-# @params model, action, params, assign_to
-#--------------------------------
-$loader->load_menu();
-
-#--------------------------------
-# Assign Layout variables
-#--------------------------------
-$loader->assign( 'title', 'RainFramework' );
-
-#--------------------------------
-# Print the layout
-#--------------------------------
-$loader->draw();
--- index.php
+++ index.php
@@ -14,3 +14,27 @@ require_once "config/directory.php";
 # Load the bootstrap
 #--------------------------------
 require_once "$app/bootstrap.php";
+
+#--------------------------------
+# Auto Load the Controller
+# init_route set the controller/action/params
+# to load the controller
+#--------------------------------
+$loader->auto_load_controller();
+
+#--------------------------------
+# Load model
+# load the model and assign the result
+# @params model, action, params, assign_to
+#--------------------------------
+$loader->load_menu();
+
+#--------------------------------
+# Assign Layout variables
+#--------------------------------
+$loader->assign( 'title', 'RainFramework' );
+
+#--------------------------------
+# Print the layout
+#--------------------------------
+$loader->draw();
