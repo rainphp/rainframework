64a037e672b90290674c99396f2bed6e0b1b4087
faryshta <angeldelcaos@gmail.com>
2012-09-25 14:35:04 -0500

    another small change on the fluent interface of bootstraps

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,9 +3,10 @@ require_once LIBRARY_DIR . "Loader.php";
 
 $loader = Loader::get_instance()
  ->init_settings() //load the settings
-//->init_db()
+ //->init_db()
  ->init_session()
  ->init_language() //set the language
-//->auth_user()
- ->init_theme(); //set theme
-// ->init_js();
+ //->auth_user()
+ ->init_theme() //set theme
+ //->init_js()
+;
