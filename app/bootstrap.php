8a08f77d85b5dbf3bfe84504c255ceece630cbd6
faryshta <angeldelcaos@gmail.com>
2012-09-24 20:42:18 -0500

    commenting the db init

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,7 +3,7 @@ require_once LIBRARY_DIR . "Loader.php";
 
 $loader = Loader::get_instance();
 $loader->init_settings();//load the settings
-$loader->init_db();
+//$loader->init_db();
 $loader->init_session();
 $loader->init_language();//set the language
 $loader->auth_user();
