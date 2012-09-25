fa70b50f10af449c05840055f7b5e3e891f683c9
faryshta <angeldelcaos@gmail.com>
2012-09-24 22:42:38 -0500

    use TPL3 instead of the previous version

--- system/library/View/Raintpl_View.php
+++ system/library/View/Raintpl_View.php
@@ -6,16 +6,20 @@
  *	Realized by Federico Ulfo & maintained by the Rain Team
  *	Distributed under MIT license http://www.opensource.org/licenses/mit-license.php
  */
-	require_once "rain.tpl.class.php";
+	require_once "Tpl.php";
 
-	class Raintpl_view extends Raintpl{
+	class Raintpl_view extends Rain\Tpl{
 		
 		var $cache = null;
 		
 		function Raintpl_View( $tpl_dir, $cache_dir, $base_url ){
-			raintpl::$tpl_dir = $tpl_dir;
-			raintpl::$cache_dir = $cache_dir;
-			raintpl::$base_url = $base_url;
+			Rain\Tpl::configure(
+			 array(
+				"tpl_dir" => $tpl_dir,
+				"cache_dir" => $cache_dir,
+				"base_url" => $base_url
+			 )
+			);
 		}
 
 		function is_cached( $tpl ){
